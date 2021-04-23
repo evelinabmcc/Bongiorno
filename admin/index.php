0b36527bea6dff98af8dd035ebe8fb349507db74
evelinabmcc <in.evelina@gmail.com>
2021-04-23 08:00:19 -0400

    enter

---
+++ admin/index.php
@@ -0,0 +1,51 @@
+<html>
+    <head>
+        <title> Bongiorno - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>   
+
+
+    <body>
+        <!-- Menu Section starts here -->
+        <div class="menu">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Admin</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Food</a></li>
+                    <li><a href="#">Order</a></li>
+
+                </ul>
+            </div>
+        </div>
+        <!-- Menu Section ends here -->
+
+
+        <!-- Dashboard Section starts here -->
+        <div class="main-content">
+            <div class="wrapper">
+                 <strong>DASHBOARD</strong>
+
+            </div>
+        </div>
+        <!-- Dashboard Section ends here -->
+
+
+        <!-- Footer Section starts here -->
+        <div class="footer">
+            <div class="wrapper">
+            <p class="text-center">
+                    All rights reserved. 
+                    Designed by <a href="#">Ryan Paul, </a>
+                                <a href="#">Taruna Mangar, and </a> 
+                                <a href="#">Evelina Iliescu-Nastasiu.</a>
+                </p>
+            </div>  
+        </div>
+        <!-- Footer Section ends here -->        
+
+    </body>
+
+</html>
---
+++ admin/login.php
@@ -0,0 +1,21 @@
+<!-- include file -->
+
+<html>
+    <head>
+        <title>Login - Food Order System</title>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        
+        <div class="login">
+            <h1 class="text-center">Login</h1>
+            <br><br>
+
+            <?php
+            ?>
+
+
+    </body>
+
+</html>
