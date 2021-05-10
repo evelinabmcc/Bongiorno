8e65440c0132e338d62ad8feb068a90708a026a4
evelinabmcc <55026430+evelinabmcc@users.noreply.github.com>
2021-05-10 15:07:59 -0400

    Update foodwebsite.sql

--- foodwebsite.sql
+++ foodwebsite.sql
@@ -1,22 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 03, 2021 at 11:10 AM
--- Server version: 5.7.24
--- PHP Version: 7.4.1
+-- Generation Time: May 10, 2021 at 01:28 PM
+-- Server version: 5.7.32
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `foodwebsite`
@@ -25,73 +17,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `admin` (
-  `admin_id` mediumint(8) UNSIGNED NOT NULL,
-  `admin_username` varchar(100) NOT NULL,
-  `admin_password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `course`
---
-
-CREATE TABLE `course` (
-  `course_id` mediumint(8) UNSIGNED NOT NULL,
-  `course_title` varchar(20) NOT NULL,
-  `availability` varchar(10) NOT NULL,
+CREATE TABLE `category` (
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `featured` varchar(10) NOT NULL,
+  `active` varchar(10) NOT NULL,
   `image_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `customer_id` mediumint(8) UNSIGNED NOT NULL,
-  `full_name` varchar(100) NOT NULL,
-  `customer_username` varchar(100) NOT NULL,
-  `customer_password` varchar(100) NOT NULL,
-  `customer_email` varchar(100) NOT NULL,
-  `customer_address` varchar(150) NOT NULL,
-  `customer_phonenumber` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `food`
---
-
-CREATE TABLE `food` (
-  `item_num` mediumint(8) UNSIGNED NOT NULL,
-  `course_id` mediumint(8) UNSIGNED NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `image` longblob NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `price` float(4,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `order_num` mediumint(8) UNSIGNED NOT NULL,
-  `customer_id` mediumint(8) UNSIGNED NOT NULL,
-  `item_num` mediumint(8) UNSIGNED NOT NULL,
-  `quantity` int(4) NOT NULL,
-  `price` float(4,2) NOT NULL,
-  `total` float(5,2) NOT NULL,
-  `order_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -99,90 +33,17 @@ CREATE TABLE `orders` (
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `category`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`admin_id`);
-
---
--- Indexes for table `course`
---
-ALTER TABLE `course`
-  ADD PRIMARY KEY (`course_id`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`);
-
---
--- Indexes for table `food`
---
-ALTER TABLE `food`
-  ADD PRIMARY KEY (`item_num`),
-  ADD KEY `course_id` (`course_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_num`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `item_num` (`item_num`);
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `admin`
-  MODIFY `admin_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `course`
---
-ALTER TABLE `course`
-  MODIFY `course_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `customer_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `food`
---
-ALTER TABLE `food`
-  MODIFY `item_num` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `order_num` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `food`
---
-ALTER TABLE `food`
-  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`course_id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`item_num`) REFERENCES `food` (`item_num`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `category`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
