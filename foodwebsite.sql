cf5046206861241714ff4e9ac784844d6b1c6ada
evelinabmcc <55026430+evelinabmcc@users.noreply.github.com>
2021-05-10 20:19:01 -0400

    Update foodwebsite.sql

--- foodwebsite.sql
+++ foodwebsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 10, 2021 at 01:28 PM
+-- Generation Time: May 10, 2021 at 07:19 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -11,8 +11,35 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
+-- Database: `evelina`
+--
+CREATE DATABASE IF NOT EXISTS `evelina` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `evelina`;
+--
 -- Database: `foodwebsite`
 --
+CREATE DATABASE IF NOT EXISTS `foodwebsite` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `foodwebsite`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` mediumint(8) UNSIGNED NOT NULL,
+  `admin_username` varchar(100) NOT NULL,
+  `admin_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_username`, `admin_password`) VALUES
+(1, 'evelina', '5f4dcc3b5aa765d61d8327deb882cf99'),
+(2, 'taruna', '5f4dcc3b5aa765d61d8327deb882cf99');
 
 -- --------------------------------------------------------
 
@@ -29,8 +56,69 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `title`, `featured`, `active`, `image_name`) VALUES
+(1, 'breakfast', 'No', 'No', 'Food_Category_139.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` mediumint(8) UNSIGNED NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `customer_username` varchar(100) NOT NULL,
+  `customer_password` varchar(100) NOT NULL,
+  `customer_email` varchar(100) NOT NULL,
+  `customer_address` varchar(150) NOT NULL,
+  `customer_phonenumber` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food`
+--
+
+CREATE TABLE `food` (
+  `item_num` mediumint(8) UNSIGNED NOT NULL,
+  `course_id` mediumint(8) UNSIGNED NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `image_name` varchar(255) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `price` float(4,2) NOT NULL,
+  `availability` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_num` mediumint(8) UNSIGNED NOT NULL,
+  `customer_id` mediumint(8) UNSIGNED NOT NULL,
+  `item_num` mediumint(8) UNSIGNED NOT NULL,
+  `quantity` int(4) NOT NULL,
+  `price` float(4,2) NOT NULL,
+  `total` float(5,2) NOT NULL,
+  `order_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `category`
@@ -39,11 +127,73 @@ ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`item_num`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_num`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `item_num` (`item_num`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `food`
+--
+ALTER TABLE `food`
+  MODIFY `item_num` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_num` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `category` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`item_num`) REFERENCES `food` (`item_num`);
