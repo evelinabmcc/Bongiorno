719d23886e36f473fa98ceb497a84d64c3fbee02
evelinabmcc <55026430+evelinabmcc@users.noreply.github.com>
2021-05-13 15:11:09 -0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!-- 
+    Authors: Ryan Paul, 
+             Taruna Mangar,
+             Evelina Iliescu-Nastasiu.
+
+     Theme:  Restaurant Online Food Ordering  
+
+     Project: Admin Panel: 'index.php'
+     
+     Date: 04.10.21
+-->
+
+<?php include('reusable-code-admin/navbar-section.php'); ?>
+
+
+        <!-- Dashboard Section starts here -->
+        <section class="main-content">
+            <div class="wrapper">
+                 <h3>DASHBOARD:</h3>
+                 
+
+                 <div class="box-4 text-center">
+                     <h2>5</h2>
+                     Categories
+                 </div>
+                
+
+                 <div class="box-4 text-center">
+                     <h2>5</h2>
+                     Categories
+                 </div>
+               
+
+                 <div class="box-4 text-center">
+                     <h2>5</h2>
+                     Categories
+                 </div>
+               
+
+                 <div class="box-4 text-center">
+                     <h2>5</h2>
+                     Categories
+                </div>
+                
+                 <div class="clearfix"></div>
+
+            </div>
+        </section>
+        <!-- Dashboard Section ends here -->
+
+
+        <?php include('reusable-code-admin/footer-section.php'); ?>
